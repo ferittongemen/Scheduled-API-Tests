94bc4b8352f315028b49c2c7bbfff3f676567af1
ferittongemen <ferittongemen@users.noreply.github.com>
2024-11-03 01:48:35 +0000

    deploy: 61025a0352977dbc1d27526e18f25092c66193c5

--- tests/test_02_get_pet.py
+++
@@ -1,9 +0,0 @@
-# tests/test_02_get_pet.py
-
-def test_get_pet(pet_page):
-    pet_id = 1
-    response = pet_page.get_pet(pet_id)
-    print(response.json())  # Yanıt içeriğini yazdırın
-    pet_page._verify_status_code(response, 200)  # Status doğrulama metodu kullanımı
-    assert response.json()["name"] == "Buddy"
-    assert response.json()["id"] == pet_id
